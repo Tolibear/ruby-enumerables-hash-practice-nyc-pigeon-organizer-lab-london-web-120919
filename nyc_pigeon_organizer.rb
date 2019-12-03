a329f5cb8dd3aeb5fe3e0f8f6e51f3a2da852bb0
T Benson <t.benson@live.co.uk>
2019-12-03 13:29:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |first_level, other|
+    other.each do |stats, names|
+      names.each do |name|
+        pigeon_list[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+# colour assignment
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      pigeon_list.keys.each do |bird|
+        if bird_name === bird
+          pigeon_list[bird][:color] << bird_color.to_s
+        end
+      end
+    end
+  end
+
+
+  pigeon_list
 end
+# binding.pry
